3ef65d0abf1c4fca726fea222b5a0b20d10261b2
Victor Hugo <victorcobain@hotmail.com>
2021-11-16 13:10:40 -0400

    jogopronto

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 const dino = document.querySelector('.dino');
-const background = document.querySelector('.backgroud');
+const background = document.querySelector('.background');
+
 let isJumping = false;
+let isGameOver = false;
+let position = 0;
 
 function handleKeyUp(event) {
-    if(event.keyCode === 32){
-        if(!isJumping) {
-        jump();
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
     }
-    }
+  }
 }
-function jump(){
-    let position = 0;
 
-    isJumping = true;
+function jump() {
+  isJumping = true;
 
-    let upInterval = setInterval(() => {
-        if(position >= 150) {
-            clearInterval(upInterval);
-            
-        // Descendo,
-        let downInterval = setInterval(() => {
-            if(position <= 0) {
-                clearInterval(downInterval);
-                isJumping = false;
-            }else{
-                position -= 20;
-                dino.style.bottom = position + 'px' ;
-            }
-            }, 20);
-                
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
         } else {
-        //subindo
-        position += 20;
-        dino.style.bottom = position + 'px' ;
+          position -= 20;
+          dino.style.bottom = position + 'px';
         }
-    }, 20);
+      }, 20);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 }
 
-function createCactus(){
-    const cactus = document.createElement('div');
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
 
-    cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
-    background.appendChild(cactus);
+  if (isGameOver) return;
 
-    let leftInterval = setInterval(() => {
-        cactusPosition -= 10;
-        cactus.style.left = cactusPosition + 'px';
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + 'px';
 
-        if (cactusPosition < -60) {
-            clearInterval(leftInterval);
-            background.removeChild(cactus);
-        }else {
-            cactusPosition -=10;
-            cactus.style.left = cactusPosition + 'px';
-        }
-    }, 20);
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
 
-    setTimeout(createCactus, randomTime);
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
